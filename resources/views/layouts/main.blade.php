b8260185e706f411b96782966b7f3a5285af1f49
Misker Birhanu <miskerbirhanu@gmail.com>
2022-04-19 11:30:08 +0300

    letter functionality is added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Core DAMS</title>
+    <title>CORE_DAMS</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('backend/asset/vendors/mdi/css/materialdesignicons.min.css')}}">
     <link rel="stylesheet" href="{{asset('backend/asset/vendors/css/vendor.bundle.base.css')}}">
